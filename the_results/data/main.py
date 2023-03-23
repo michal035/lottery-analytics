8ac7696473c5903c63b81262eb0e769e3f66378a
Michal <mwiselka035@gmail.com>
2023-03-23 22:36:39 +0100

    returns number of occurrences of certain number through the all lotteries

--- the_results/data/main.py
+++ the_results/data/main.py
@@ -7,23 +7,48 @@ df =  df[df['date'].isnull() == False]
 duplicated_date = df.duplicated()
 df = df[duplicated_date==False]
 
+df.to_csv("the_results/re2.csv")
 
 
+def a(df):
+    #there is 49 numbers
+    #for i in range(48):
+    
+    counter = 0
+    for i in df.columns:
+        temp_df = df.loc[df[i] == '24']
 
-def duplicates(df):
-    collumn_name = list(df.columns)[0]
-    for i in df[collumn_name]:
-        pass
-        
+        if len(temp_df) != 0:
+            print(f"{temp_df} \n")
+            print(f"{len(temp_df)} {i} \n")
+
+        counter += len(temp_df)
+
+
+    print(counter)
+   
+
+def b(df):
+     for i in df.columns:
+         print(df[i])
 
 
 lotto = (df["lotto"]).to_frame()
-#duplicates(lotto)
+
 
 lotto["numbers"] =  df["lotto"].str.split(" ")
-print(lotto)
+
 
 
 # each number is it's own collumn - I don't think this will be that useful
-#lotto = pd.DataFrame(lotto.lotto.str.split(' ').tolist(),columns = ['1','2','3','4','5','6'])
+lotto2 = pd.DataFrame(lotto.lotto.str.split(' ').tolist(),columns = ['1','2','3','4','5','6'])
+
+
+
+a(lotto2)
+
+
+
+#Alternative might be useful later ig 
+#b(lotto2)
 
