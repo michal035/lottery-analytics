afc878d033e94fab89484201036b7b6bc955733f
Michal <mwiselka035@gmail.com>
2023-03-28 19:14:39 +0200

    better graph?

--- the_results/data/graphs.py
+++ the_results/data/graphs.py
@@ -1,15 +1,23 @@
 import pandas as pd 
-import matplotlib as plt
+import matplotlib.pyplot as plt
 from main import data_for_further_processing
 
 
 df = data_for_further_processing()[1]
 
-df = df.sort_values(by="number")
 
-#just a random test graph
-df.plot(x=df.columns[0], y=df.columns[1], kind='line')
+df['number'] = df['number'].astype(str)
+df = df.sort_values(by='occurences', ascending=False)
 
 
-plt.pyplot.show()
+colors = ['#ff9999', '#66b3ff', '#99ff99', '#ffcc99', '#ffb3e6']
+plt.barh(df['number'], df['occurences'], color=colors)
 
+
+
+plt.xlabel('number of occurences')
+plt.ylabel('Numbers')
+plt.title('How many times each number got pick')
+
+
+plt.show()
