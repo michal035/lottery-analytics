f8513555675efff6301a99985c10ab35827df9a0
Michal <mwiselka035@gmail.com>
2023-04-05 20:11:59 +0200

    Got it counting the difference in days between each time the number was picked

--- the_results/data/dates.py
+++ the_results/data/dates.py
@@ -1,5 +1,6 @@
 import pandas as pd 
 from main import get_dates
+import datetime
 
 
 df = get_dates()
@@ -8,7 +9,31 @@ dates = df[df["number"] == "1"]
 dates = pd.DataFrame(((dates["info"].str.split(' ')).to_list())[0],columns = ["dates"])
 dates["dates"] = pd.to_datetime(dates["dates"])
 
-#dates["dates"] = dates["dates"].sort_values(inplace=True)
+dates = dates.sort_values("dates")
+
+
+def counting():
+
+    o_year = 0
+    counter = 0
+    sum_od_days = 0
+    previos_date = None
+
+    for i in (dates["dates"].tolist()):
+        
+        if previos_date != None:
+            num_of_days = (i - previos_date).days  
+            print(num_of_days)
+        
+        previos_date = i
+       
+                
+
+
+counting()
+
+#print(dates.iloc[2,0])
+
 
 
 """
