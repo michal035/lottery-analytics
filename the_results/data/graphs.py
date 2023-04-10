af665ef7e894f1553a4721cf52d9dabe8dd3c3ad
Michal <mwiselka035@gmail.com>
2023-04-10 17:32:28 +0200

    added a graph showing the number of days it takes on average for a number to be picked again

--- the_results/data/graphs.py
+++ the_results/data/graphs.py
@@ -1,9 +1,26 @@
 import pandas as pd 
 import matplotlib.pyplot as plt
+import seaborn as sns
 from main import get_main
+from dates import get_dates_results
 
 
-df = get_main()[1]
+
+df2 = get_dates_results()
+sub_df1 = df2[["number","avrage_time_between"]]
+
+
+
+sns.set_style("darkgrid")  
+sns.barplot(x='number', y='avrage_time_between', data=sub_df1, palette='Blues_d') 
+plt.title("On avarge how long it takes for number to be picked again")  
+plt.xlabel("number") 
+plt.ylabel("Number of days")  
+plt.show()
+
+
+#just some basic graph showing how many times each number occured 
+"""df = get_main()[1]
 
 
 df['number'] = df['number'].astype(str)
@@ -18,6 +35,8 @@ plt.barh(df['number'], df['occurences'], color=colors)
 plt.xlabel('number of occurences')
 plt.ylabel('Numbers')
 plt.title('How many times each number got pick')
+"""
+
+#plt.show()
 
 
-plt.show()
