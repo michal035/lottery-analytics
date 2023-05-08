98ed821807093d54b9bfa918ca4f9e0e41eb5f7a
Michal <mwiselka035@gmail.com>
2023-05-08 18:02:51 +0200

    implemented custom labels to every graph

--- the_results/data/graphs.py
+++ the_results/data/graphs.py
@@ -8,37 +8,37 @@ from datetime import datetime
 
 df2 = get_dates_results()
 
-# This is basically so that numbers don't go on top of each other 
-def offset_for_labeles(ax, offset= -16):
-    #offset = -16  
-    for i, label in enumerate(ax.get_xticklabels()):
-    
-        y = offset
-        ax.text(i, y, label.get_text(), ha='center', va='bottom', fontsize=10)
-        
-        offset = - 16
-        if i%2 == 1:
-            d = 0
-        else:
-            d = 6
-
-        offset += d
-        
-    ax.set_xticklabels([])
-
-
 
 sub_df1 = df2[["number","average_time_between"]]
 
 sns.set_style("darkgrid")  
-ax = sns.barplot(x='number', y='average_time_between', data=sub_df1, palette='Blues_d') 
+cx = sns.barplot(x='number', y='average_time_between', data=sub_df1, palette='Blues_d') 
 plt.title("On average how long it takes for number to be picked again")  
-plt.xlabel("number", labelpad=16) 
+plt.xlabel("number",labelpad=19) 
 plt.ylabel("Number of days")  
 
-offset_for_labeles(ax,-10)
-plt.savefig('the_results/graphs/average_number_od_days.png')
 
+# This is the code responsible for the custom assignment of labels for each bar in a way that ensures that 
+# adjacent bars' labels do not overlap.
+
+offset = -11
+for i, label in enumerate(cx.get_xticklabels()):
+   
+    x = i
+    y = offset
+    cx.text(x, y, label.get_text(), ha='center', va='bottom', fontsize=10)
+    
+    offset = - 11
+    if i%2 == 1:
+        d = 0
+    else:
+        d = 6
+
+    offset += d
+    
+cx.set_xticklabels([])
+
+plt.savefig('the_results/graphs/average_number_od_days.png')
 #plt.show()
 plt.clf()
 
@@ -51,13 +51,31 @@ df = df.sort_values(by='occurences', ascending=False)
 sub_df3 = df[['number','occurences']]
 
 sns.set_style("darkgrid")  
-ax = sns.barplot(x='number', y='occurences', data=sub_df3, palette='Oranges_r') 
-plt.xlabel('number of occurences', labelpad=16)
+bx = sns.barplot(x='number', y='occurences', data=sub_df3, palette='Oranges_r') 
+plt.xlabel('number of occurences',labelpad=18)
 plt.ylabel('Numbers')
 plt.title('How many times each number got pick')
 
-#plt.xticks(rotation=90)
-offset_for_labeles(ax)
+
+
+offset = - 2.5
+for i, label in enumerate(bx.get_xticklabels()):
+   
+    x = i
+    y = offset
+    bx.text(x, y, label.get_text(), ha='center', va='bottom', fontsize=10)
+    
+    offset = - 2.5
+    if i%2 == 1:
+        d = 0
+    else:
+        d = 1
+
+    offset += d
+    
+bx.set_xticklabels([])
+
+
 plt.savefig('the_results/graphs/basic_graph_number_of_picks.png')
 #plt.show()
 plt.clf()
@@ -74,7 +92,24 @@ plt.xlabel("numbers",labelpad=16)
 plt.ylabel("Number of days")
 
 
-offset_for_labeles(ax)
+
+offset = -16  
+for i, label in enumerate(ax.get_xticklabels()):
+   
+    x = i
+    y = offset
+    ax.text(x, y, label.get_text(), ha='center', va='bottom', fontsize=10)
+    
+    offset = - 16
+    if i%2 == 1:
+        d = 0
+    else:
+        d = 6
+
+    offset += d
+    
+    
+ax.set_xticklabels([])
 
 
 plt.savefig('the_results/graphs/days_since_last_pick.png')
